e6ba00604527dadaf3481a657fae4315b3b74aea
AvanAvi <theastrofreek@gmail.com>
2024-06-01 15:14:51 +0200

    Implement Book entity and BookTest
    - Add constructor, getter, and setter methods to the Book class
    - Update BookTest to use the constructor and getter methods
    - Ensure the testBookCreation test passes

--- library-app/src/main/java/com/library/app/entity/Book.java
+++ library-app/src/main/java/com/library/app/entity/Book.java
@@ -7,6 +7,36 @@ public class Book {
     private String isbn;
     private int publicationYear;
 
+    // Constructor
+    public Book(Long id, String title, String author, String isbn, int publicationYear) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+    }
+
+    // Getter methods
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
     // Setter methods
     public void setId(Long id) {
         this.id = id;
